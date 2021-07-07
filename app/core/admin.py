f47b3d59f3dfa2ac060faafd615f9ac48bad7817
arifhusaini97 <35693271+arifhusaini97@users.noreply.github.com>
2021-07-07 08:55:54 +0800

    Update custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext as _
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
-    fieldsets: (
+    fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
         (
@@ -21,6 +21,13 @@ class UserAdmin(BaseUserAdmin):
             }
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+
+        }),
     )
 
 
