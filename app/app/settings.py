60507fa687c370c2b59e72e6662a8af6884aae69
arifhusaini97 <35693271+arifhusaini97@users.noreply.github.com>
2021-07-11 17:30:26 +0800

    Check with instructor.

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,7 +128,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# 127.0.0.1:8000/static/
 STATIC_URL = '/static/'
+# 127.0.0.1:8000/media/
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
